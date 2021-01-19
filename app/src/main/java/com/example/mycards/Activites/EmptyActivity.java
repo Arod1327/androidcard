387d3eddeb6144685c8609fe2cfd34cf0167ec94
Arod1327 <76719882+Arod1327@users.noreply.github.com>
2021-01-18 21:35:56 -0600

    Inicio del proyecto

---
+++ app/src/main/java/com/example/mycards/Activites/EmptyActivity.java
@@ -0,0 +1,15 @@
+package com.example.mycards.Activites;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mycards.R;
+
+public class EmptyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);//Cambiar por su layout
+    }
+}
---
+++ app/src/main/java/com/example/mycards/Activites/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mycards.Activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.mycards.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"La app funciona de manera local, es necesario otorgar permiso de lectura de dispositivo",Toast.LENGTH_LONG).show();
+    }
+}
