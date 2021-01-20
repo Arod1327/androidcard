f179f24dc651b61f15da0baa05c84fd1003c2a65
Arod1327 <76719882+Arod1327@users.noreply.github.com>
2021-01-20 15:03:38 -0600

    Update EmptyActivity.java
    Se solicita el uso de Shared Preferences en la aplicación móvil.

--- app/src/main/java/com/example/mycards/Activites/EmptyActivity.java
+++ app/src/main/java/com/example/mycards/Activites/EmptyActivity.java
@@ -11,5 +11,6 @@ public class EmptyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//Cambiar por su layout
+        //usar shared preferences
     }
 }
